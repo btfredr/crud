4e527f095c8bd261d29d884a7503788de95701c2
Fredrik <fredrikf2000@gmail.com>
2021-03-09 13:05:02 +0100

    login now works, added router in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={HomePage}></Route>
+        <Route path='/login' exact component={Login}></Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,14 +2,32 @@ import { useState } from 'react';
 import { loginSchema } from '../utils/schemas';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { BASE_URL, AUTH_PATH } from '../utils/constants';
+import axios from 'axios';
 
 const Login = () => {
     const [submitting, setSubmitting] = useState(false);
     const [loginError, setLoginError] = useState(null);
-
     const { register, handleSubmit, errors } = useForm({
         resolver: yupResolver(loginSchema)
     });
+
+    const onSubmit = async (data) => {
+        setSubmitting(true);
+        setLoginError(null);
+
+        console.log(data);
+
+        try {
+            const response = await axios.post(`${BASE_URL}${AUTH_PATH}`, data);
+            console.log('response', response.data);
+        } catch (error) {
+            console.log('error', error);
+            setLoginError(error.toString());
+        } finally {
+            setSubmitting(false);
+        }
+    };
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit)}>
@@ -20,9 +38,10 @@ const Login = () => {
                         {errors.identifier && <p>{errors.identifier.message}</p>}
                     </div>
                     <div>
-                        <input name='identifier' placeholder='Username' ref={register} type='password' />
+                        <input name='password' placeholder='Password' ref={register} type='password' />
                         {errors.password && <p>{errors.identifier.password}</p>}
                     </div>
+                    <button type='submit'>{submitting ? 'Logging in...' : 'Login'}</button>
                 </fieldset>
             </form>
         </>
