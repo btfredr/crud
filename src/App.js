a6dde6da97b940c6f25d8b0aeff53e5f2349324d
Fredrik <fredrikf2000@gmail.com>
2021-03-09 13:32:35 +0100

    fixed typos that caused app to break

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' exact component={HomePage}></Route>
-        <Route path='/login' exact component={Login}></Route>
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={HomePage}></Route>
+          <Route path='/login' exact component={Login}></Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { loginSchema } from '../utils/schemas';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { BASE_URL, AUTH_PATH } from '../utils/constants';
 import axios from 'axios';
+import AuthContext from '../context/AuthContext';
 
 const Login = () => {
     const [submitting, setSubmitting] = useState(false);
@@ -11,6 +12,7 @@ const Login = () => {
     const { register, handleSubmit, errors } = useForm({
         resolver: yupResolver(loginSchema)
     });
+    const [, setAuth] = useContext(AuthContext)
 
     const onSubmit = async (data) => {
         setSubmitting(true);
@@ -21,6 +23,7 @@ const Login = () => {
         try {
             const response = await axios.post(`${BASE_URL}${AUTH_PATH}`, data);
             console.log('response', response.data);
+            setAuth(response.data);
         } catch (error) {
             console.log('error', error);
             setLoginError(error.toString());
