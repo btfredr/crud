5a0eca1a285ae807e6de8482cf94b1dce7cce99b
Fredrik <fredrikf2000@gmail.com>
2021-03-10 13:59:55 +0100

    added functionality to edit products

--- src/pages/EditProduct.js
+++ src/pages/EditProduct.js
@@ -1,37 +1,114 @@
-import  {useParams} from 'react-router-dom';
-import useAxios from '../utils/useAxios';
-import {useState, useEffect} from 'react';
-import Item from '../components/Item';
-import {PRODUCTS_PATH} from '../utils/constants';
+import { useParams } from "react-router-dom";
+import useAxios from "../utils/useAxios";
+import { useState, useEffect } from "react";
+import Item from "../components/Item";
+import { PRODUCTS_PATH } from "../utils/constants";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { productSchema } from "../utils/schemas";
 
 const EditProduct = () => {
-    const [product, setProduct] = useState(null);
-    const {id} = useParams();
-    const http = useAxios();
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
+  const http = useAxios();
 
-    useEffect(() => {
-        const getProduct = async () => {
-            try{
-                const response = await http.get(`${PRODUCTS_PATH}/${id}`);
-                console.log(response);
-                setProduct(response.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getProduct();
-    }, [id]);
+  const [submitting, setSubmitting] = useState(false);
+  const [updateError, setUpdateError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
-    if (!product) {
-        return <p>Loading product...</p>
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(productSchema),
+  });
+
+  const onSubmit = async (data) => {
+    setSubmitting(true);
+    setUpdateError(null);
+    console.log(data);
+    try {
+      const response = await http.put(`${PRODUCTS_PATH}/${id}`, data);
+      console.log(response);
+      setProduct(response.data);
+      setSuccess(true);
+    } catch (error) {
+      console.log("error", error);
+      setUpdateError(error.toString());
+    } finally {
     }
+  };
 
-    return (
-        <>
-        <h2>Edit</h2>
-        <Item {...product} />
-        </>
-    )
-}
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await http.get(`${PRODUCTS_PATH}/${id}`);
+        console.log(response);
+        setProduct(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProduct();
+  }, [id]);
 
-export default EditProduct
+  if (!product) {
+    return <p>Loading product...</p>;
+  }
+
+  return (
+    <>
+      <h2>Edit</h2>
+      <Item {...product} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {updateError && <p>{updateError}</p>}
+        <fieldset disabled={submitting}>
+          <div>
+            <input
+              name="title"
+              placeholder="Title"
+              ref={register}
+              defaultValue={product.title}
+            />
+            {errors.title && <p>{errors.identifier.message}</p>}
+          </div>
+
+          <div>
+            <input
+              name="price"
+              placeholder="Price"
+              defaultValue={product.price}
+              ref={register}
+              type="number"
+            />
+            {errors.price && <p>{errors.price.message}</p>}
+          </div>
+          <div>
+            <textarea
+              name="description"
+              placeholder="Description"
+              defaultValue={product.description}
+              ref={register}
+              type="text"
+            />
+            {errors.description && <p>{errors.description.message}</p>}
+          </div>
+          <div>
+            <input
+              name="image_url"
+              placeholder="Image URL"
+              ref={register}
+              defaultValue={product.image_url}
+              type="text"
+            />
+            {errors.image_url && <p>{errors.image_url.message}</p>}
+          </div>
+
+          <button type="submit">
+            {submitting ? "Updating ..." : "Update"}
+          </button>
+        </fieldset>
+      </form>
+      {success ? <p>Listing of {product.title} was updated</p> : null}
+    </>
+  );
+};
+
+export default EditProduct;
--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -4,3 +4,10 @@ export const loginSchema = yup.object().shape({
     identifier: yup.string().required('Please enter your username'),
     password: yup.string().required('Please enter your password')
 });
+
+export const productSchema = yup.object().shape({
+    title: yup.string().required('Please enter a title'),
+    price: yup.number().required('Please enter a title'),
+    image_url: yup.string().required('Please enter a title'),
+    description: yup.string().required('Please enter a title')
+});
