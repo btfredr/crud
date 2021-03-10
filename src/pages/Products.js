81d16e4641d9ff648d0d7090ce0cba0d237d4d09
Fredrik <fredrikf2000@gmail.com>
2021-03-10 12:54:52 +0100

    added constants to products page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,24 @@
+import { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+import useAxios from '../utils/useAxios';
+
 export const Products = () => {
+    const [auth] = useContext(AuthContext);
+    const history = useHistory();
+    const [products, setProducts] = useState(null);
+    const http = useAxios();
+
+    if (!auth) {
+        history.push('/login');
+    }
+
+
     return (
-        <h1>Products</h1>
-    )
-}
+        <>
+            <h1>Products</h1>
+        </>
+    );
+};
 
 export default Products;
