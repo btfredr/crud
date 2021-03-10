aa9a14df675d7310cc0eaab689979c6ed2b47b5d
Fredrik <fredrikf2000@gmail.com>
2021-03-10 12:59:17 +0100

    code now fetches products from API

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,12 +2,26 @@ import { useContext, useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 import useAxios from '../utils/useAxios';
+import { PRODUCTS_PATH } from '../utils/constants';
 
 export const Products = () => {
     const [auth] = useContext(AuthContext);
     const history = useHistory();
     const [products, setProducts] = useState(null);
     const http = useAxios();
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const response = await http.get(PRODUCTS_PATH);
+                console.log(response);
+                setProducts(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getProducts();
+    }, [])
 
     if (!auth) {
         history.push('/login');
