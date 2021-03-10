ef36d4a6d2579ccff5af01850bb576de61ca443c
Fredrik <fredrikf2000@gmail.com>
2021-03-10 13:33:10 +0100

    added edit page and link to singular items

--- src/pages/EditProduct.js
+++ src/pages/EditProduct.js
@@ -1,5 +1,37 @@
+import  {useParams} from 'react-router-dom';
+import useAxios from '../utils/useAxios';
+import {useState, useEffect} from 'react';
+import Item from '../components/Item';
+import {PRODUCTS_PATH} from '../utils/constants';
+
 const EditProduct = () => {
-    return <h2>Edit this product</h2>
+    const [product, setProduct] = useState(null);
+    const {id} = useParams();
+    const http = useAxios();
+
+    useEffect(() => {
+        const getProduct = async () => {
+            try{
+                const response = await http.get(`${PRODUCTS_PATH}/${id}`);
+                console.log(response);
+                setProduct(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getProduct();
+    }, [id]);
+
+    if (!product) {
+        return <p>Loading product...</p>
+    }
+
+    return (
+        <>
+        <h2>Edit</h2>
+        <Item {...product} />
+        </>
+    )
 }
 
 export default EditProduct
