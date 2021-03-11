a655bc49deba111185f0fe2c4b26ccafc54c0491
Fredrik <fredrikf2000@gmail.com>
2021-03-11 16:47:28 +0100

    fixed messages and styling

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -1,13 +1,13 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 export const loginSchema = yup.object().shape({
-    identifier: yup.string().required('Please enter your username'),
-    password: yup.string().required('Please enter your password')
+  identifier: yup.string().required("Please enter your username"),
+  password: yup.string().required("Please enter your password"),
 });
 
 export const productSchema = yup.object().shape({
-    title: yup.string().required('Please enter a title'),
-    price: yup.number().required('Please enter a title'),
-    image_url: yup.string().required('Please enter a title'),
-    description: yup.string().required('Please enter a title')
+  title: yup.string().required("Please enter a title"),
+  price: yup.number().required("Please enter a price"),
+  image_url: yup.string().required("Please enter an image URL"),
+  description: yup.string().required("Please enter a description"),
 });
